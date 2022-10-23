4b1538d888906423e1db871c4a07260e093f1fec
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-10-23 10:45:57 +0700

    update main ín abstac data type

--- AbstractDataTypeAndListADT/src/com/company/Main.java
+++ AbstractDataTypeAndListADT/src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int n = 10;
+        SimpleArrayList list = new SimpleArrayList(n);
+        /*SimpleLinkedList list = new SimpleLinkedList(n);*/
+
+        //add to list
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        //display
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+        //delete
+        int value = 0;
+        list.remove(value);
+        //display
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+        //set value
+        int idx = 5;
+        value = 15;
+        list.set(idx, value);
+        //display
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+
     }
 }
