8352ae9633d15a48141e913c4a7f4daa055bac0f
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-10-23 10:52:35 +0700

    .

--- StackQueue/src/com/company/Main.java
+++ StackQueue/src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.lang.reflect.Array;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        ArrayQueue queue = new ArrayQueue();
+        /*LinkedListQueue queue = new LinkedListQueue();*/
+        LinkedListStack stack = new LinkedListStack();
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            queue.enqueue(i);
+            stack.push(i);
+        }
+        System.out.println("QUEUE");
+        for (int i = 0; i < n; i++) {
+            System.out.print(queue.dequeue());
+        }
+        System.out.println();
+        System.out.println("STACK");
+        for (int i = 0; i < n; i++) {
+            System.out.print(stack.pop());
+        }
     }
 }
