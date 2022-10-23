c4617c9bcb6deed5117489b63fd7450bb122ffad
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-10-23 11:43:40 +0700

    Update Main.java

--- SimpleSort/src/com/company/Main.java
+++ SimpleSort/src/com/company/Main.java
@@ -1,15 +1,18 @@
 package com.company;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner scanner = new Scanner(System.in);
         SimpleSort simpleSort = new SimpleSort();
-        int[] arr = new int[20];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = new Random().nextInt();
+        int n = 10;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(scanner.nextLine());
         }
         //bubble sort
         /*simpleSort.bubbleSort(arr);*/
